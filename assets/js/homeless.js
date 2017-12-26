ac8c066d7297c3a7a94dad813a404eb7c44f0480
Mica Meintel-Wade <mica.meintel-wade@fiscal.treasury.gov>
2017-12-26 15:46:59 -0500

    update homeless.js

--- assets/js/homeless.js
+++ assets/js/homeless.js
@@ -190,7 +190,7 @@ d3.json('/data-lab-data/2017_CoC_Grantee_Areas_2.json', function(us) {
                     .html(function(d) {
                       return "<b>" + d.properties.COCNAME + "</b>" + "<br>" +
                         "Continuum of Care Number: " + d.properties.coc_number + "<br>" +
-                        "Total Homeless: " + data[i].pop;
+                        "Total Homeless: " + data[i].total_homeless;
                     });
 
                   map_svg.call(tip)
