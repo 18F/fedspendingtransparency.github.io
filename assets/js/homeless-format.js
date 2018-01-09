3c05b8ffd0c1ca2f7e8176a5cc66f703ddfea004
Mica Meintel-Wade <mica.meintel-wade@fiscal.treasury.gov>
2018-01-09 12:58:47 -0500

    fix boxes
    now they can both be open

--- assets/js/homeless-format.js
+++ assets/js/homeless-format.js
@@ -13,17 +13,17 @@ function openTab(evt, tabName) {
 }
 document.getElementById("defaultOpen").click();
 
-function openTab2(evt, tab2Name) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+function openTab2(evt, tabName) {
+    var i, tabcontent2, tablinks2;
+    tabcontent2 = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i < tabcontent2.length; i++) {
+        tabcontent2[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks2 = document.getElementsByClassName("tablinks2");
+    for (i = 0; i < tablinks2.length; i++) {
+        tablinks2[i].className = tablinks2[i].className.replace(" active", "");
     }
-    document.getElementById(tab2Name).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen2").click();
