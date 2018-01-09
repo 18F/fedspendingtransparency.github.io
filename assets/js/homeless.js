32cb217c1969ce81dc8737204c1ea876305d5408
Mica Meintel-Wade <mica.meintel-wade@fiscal.treasury.gov>
2018-01-08 20:06:11 -0500

    Update homeless piece
    update data lab page, ad modal

--- assets/js/homeless.js
+++ assets/js/homeless.js
@@ -4,7 +4,7 @@
 //
 //
 // Spinner loading controls
-function openTab(evt, tabName) {
+/*function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -34,6 +34,31 @@ function openTab2(evt, tab2Name) {
 }
 document.getElementById("defaultOpen2").click();
 
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+*/
 var opts = {
   lines: 9, // The number of lines to draw
   length: 16, // The length of each line
